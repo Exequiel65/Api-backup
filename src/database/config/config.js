14a7333048509e111a9389417e995f489da2b95d
Exequiel65 <marcos97britos@gmail.com>
2022-03-29 23:24:54 -0300

    env

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER2,
+    "password": process.env.DB_PASSWORD2,
+    "database": process.env.DB_NAME2,
+    "host": process.env.DB_HOST2,
     "dialect": "mysql"
   }
 }
